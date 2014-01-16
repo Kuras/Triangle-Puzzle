70a3fa061917bcd93fd1f58207074d1a41397c33
Przemek Kura <kuraprzemek@gmail.com>
2014-01-16 20:08:51 +0100

    create new repo for Triangle puzzle project

---
+++ Triangle/Controller/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 16 sty 2014
+ *      Author: Baraka
+ */
+
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main (void){
+
+	return EXIT_SUCCESS;
+}
+
+
+
+
+
+
